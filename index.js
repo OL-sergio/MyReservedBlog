d29e99d1ffe126de63d7ad761ac7c97133f2962b
Sérgio Oliveira <a18074@alunos.ipca.pt>
2026-07-09 13:30:32 +0100

    Work Done:
    - Change the port and host to work on Render hosting.
    
    Notes:
    
    -...
    
    Add items:
    -...
    
    Updated items:
    -...
    
    Removed:
    -...

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import { ArticleModel } from './public/js/articles-manager.js';
 const app = express();
 // TODO: render.com provides the PORT and HOST environment variables,
 // so we need to use those instead of hardcoding them
-//const PORT = process.env.PORT || 10000;
-//const HOST = process.env.HOST || '0.0.0.0';
+ const PORT = process.env.PORT || 10000;
+ const HOST = process.env.HOST || '0.0.0.0';
 
 // TODO: For local development, you can uncomment the lines
 //  below and comment out the lines above
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || '0.0.0.0';
+// const PORT = process.env.PORT || 3000;
+// const HOST = process.env.HOST || '0.0.0.0';
 
 // Middleware
 app.set('view engine', 'ejs');
